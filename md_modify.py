8cd759337e3fd3aaa2f717fcfdb3d9523e86ebc7
dalong <dalong0514@126.com>
2020-12-03 15:18:24 +0800

    yellow hat - md_modify

--- md_modify.py
+++ md_modify.py
@@ -1,20 +1,27 @@
 # -*- coding:utf-8 -*-
 
-import glob, os
-
+import glob, os, time
 import modify as md
 
-# 获得桌上所有 md 文件名并对其进行分割，然后在 for 循环里进行处理
-for infile in glob.glob("/Users/Daglas/Desktop/*.md"):
-    filename, ext = os.path.splitext(infile)
+def modify_file_content():
+    # 获得桌上所有 md 文件名并对其进行分割，然后在 for 循环里进行处理
+    for infile in glob.glob("/Users/Daglas/Desktop/*.md"):
+        filename, ext = os.path.splitext(infile)
 
-    # 读取文件，文件名「filename + ".md"」是关键
-    with open(filename + ".md") as file_obj:
-        lines = file_obj.readlines()
+        # 读取文件，文件名「filename + ".md"」是关键
+        with open(filename + ".md") as file_obj:
+            lines = file_obj.readlines()
 
-    # 对文字处理并写入文件
-    with open(filename + ".md", 'w') as file_obj:
-        for line in lines:
-            if line != '\n':
-                new_content = md.modify_text(line)
-                file_obj.write(new_content + "\n\n")
+        # 对文字处理并写入文件
+        with open(filename + ".md", 'w') as file_obj:
+            for line in lines:
+                if line != '\n':
+                    new_content = md.modify_text(line)
+                    file_obj.write(new_content + "\n\n")
+
+if __name__ == '__main__':
+    start_time = time.time()
+    modify_file_content()
+    end_time = time.time()
+    print('OK!')
+    print('Time Used: ' + str(end_time - start_time) + 's')
