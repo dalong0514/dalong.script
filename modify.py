23725b0c8c2d8364463af1e4270354520debf0f7
dalong <dalong0514@126.com>
2020-12-03 15:34:15 +0800

    yellow hat - modify.py

--- modify.py
+++ modify.py
@@ -3,10 +3,8 @@ import pangu
 
 def modify_text(line):
     """处理文字的格式"""
-    
     # 去 \n 是转 pdf 时启用
     # line = line.replace('\n', '')
-
     line = pangu.spacing_text(line)
     line = line.replace(' “', '“')
     line = line.replace('” ', '”')
@@ -15,4 +13,5 @@ def modify_text(line):
     line = line.replace('・', '·')
     line = line.replace('， ', '，')
     line = line.replace('。 ', '。')
+    line = line.strip()
     return line
