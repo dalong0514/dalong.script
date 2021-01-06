f37655800b5e32082f218b16296982ae481605ea
dalong <dalong0514@126.com>
2021-01-06 23:04:26 +0800

    yellow hat - jpeg_modify

--- jpeg_modify.py
+++ jpeg_modify.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
-import glob, os
-
+import glob, os, time
 from PIL import Image
 
-# 手动获取需要设置的宽度像素
-user_width = int(input("Please enter the image's width: "))
+def jpeg_modify():
+    # 手动获取需要设置的宽度像素
+    user_width = int(input("Please enter the image's width: "))
+    # 获取该文件夹下所有特定文件的所有对象并分隔开来
+    for infile in glob.glob("/Users/Daglas/Desktop/*.jpeg"):
+        file, ext = os.path.splitext(infile)
+        im = Image.open(infile)
+        # 只对宽度像素大于给定值的图片进行处理
+        if im.width > user_width:
+            new_size = int(user_width), int(user_width*(im.height/im.width))
+            im = im.resize(new_size)
+            im.save(file + ".jpeg")
 
-# 获取该文件夹下所有特定文件的所有对象并分隔开来
-for infile in glob.glob("/Users/Daglas/Desktop/*.jpeg"):
-    file, ext = os.path.splitext(infile)
-    im = Image.open(infile)
-
-    # 只对宽度像素大于给定值的图片进行处理
-    if im.width > user_width:
-        new_size = int(user_width), int(user_width*(im.height/im.width))
-        im = im.resize(new_size)
-        im.save(file + ".jpeg")
+if __name__ == '__main__':
+    time1=time.time()
+    jpeg_modify()
+    time2 = time.time()
+    print('OK!')
+    print('Time Used: ' + str(time2 - time1) + 's')
--- num_image.py
+++ num_image.py
@@ -3,10 +3,8 @@ import glob, os, time
 from PIL import Image
 
 def num_image():
-
     # 手动获取需要设置的宽度像素
     init_num = int(input("Please enter the init number: "))
-
     # 手动获取图片的格式
     image_format = input("Please enter the image's format(jpg/jpeg/png-default): ")
 
@@ -20,15 +18,12 @@ def num_image():
     if user_input != '':
         user_width = int(user_input)
     # else:
-    #     user_width = 800
-
+    # user_width = 800
     all_files = glob.glob("/Users/Daglas/Desktop/*." + im_format)
-
     # 获取该文件夹下所有特定文件的所有对象并分隔开来
     for infile in sorted(all_files):
         file, ext = os.path.splitext(infile)
         im = Image.open(infile)
-
         # 只对宽度像素大于给定值的图片进行处理
         if user_input != '' and im.width > user_width:
             new_size = int(user_width), int(user_width*(im.height/im.width))
@@ -36,7 +31,6 @@ def num_image():
             im.save("/Users/Daglas/Desktop/" + str(init_num) + "." + im_format)
         else:
             im.save("/Users/Daglas/Desktop/" + str(init_num) + "." + im_format)
-
         init_num += 1
 
 if __name__ == '__main__':
