8f3a9468adcff6a9af439fa7a444bf5ce7b483ab
dalong <dalong0514@126.com>
2021-10-19 08:29:02 +0800

    yellow hat - refactor md_modify.py

--- md_modify.py
+++ md_modify.py
@@ -3,21 +3,24 @@
 import glob, os, time
 import modify as md
 
+
 def modify_file_content():
     # 获得桌上所有 md 文件名并对其进行分割，然后在 for 循环里进行处理
-    for infile in glob.glob("/Users/Daglas/Desktop/*.md"):
-        filename, ext = os.path.splitext(infile)
+    fir_dir = "/Users/Daglas/Desktop/*.md"
+    # fir_dir = "C:\\Users\\dell\\Desktop\\mdFiles\\*.md"
 
-        # 读取文件，文件名「filename + ".md"」是关键
-        with open(filename + ".md") as file_obj:
+    for infile in glob.glob(fir_dir):
+        file_name, ext = os.path.splitext(infile)
+        # 读取文件，文件名「file_name + ".md"」是关键
+        with open(file_name + ".md", encoding='UTF-8') as file_obj:
             lines = file_obj.readlines()
-
         # 对文字处理并写入文件
-        with open(filename + ".md", 'w') as file_obj:
+        with open(file_name + ".md", 'w', encoding='UTF-8') as file_obj:
             for line in lines:
                 if line != '\n':
                     new_content = md.modify_text(line)
                     file_obj.write(new_content + "\n\n")
+
 
 if __name__ == '__main__':
     start_time = time.time()
