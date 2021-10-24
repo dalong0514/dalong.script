062816de438a2124ce0b7b9348b488d721992d34
dalong <dalong0514@me.com>
2021-10-24 13:52:25 +0800

    yellow hat - refactor split_file.py

--- split_file.py
+++ split_file.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import glob, os, time
-import math
-import pandas as pd
 import modify as md
+
 
 # split the arr into N chunks
 def chunks(arr, in_list):
@@ -17,21 +16,24 @@ def chunks(arr, in_list):
     for v in seq:
         if seq.index(v) < len(seq) - 1:
             tips.append(arr[v:seq[seq.index(v)+1]])
-    
+
     tips.append(arr[seq[-1]:])
     return tips
 
-def SplitFiles():
+
+def split_files():
+    fir_dir = "/Users/Daglas/Desktop/*.txt"
+    # fir_dir = "C:\\Users\\dell\\Desktop\\mdFiles\\*.txt"
     # 获得桌上所有 txt 文件名并对其进行分割，然后在 for 循环里进行处理
-    for infile in glob.glob("/Users/Daglas/Desktop/*.txt"):
+    for infile in glob.glob(fir_dir):
         filename, ext = os.path.splitext(infile)
 
         # 读取文件
-        with open(filename + ".txt") as file_obj:
+        with open(filename + ".txt", encoding='UTF-8') as file_obj:
             lines = file_obj.readlines()
 
         # 对文字处理并写入文件
-        with open(filename + ".txt", 'w') as file_obj:
+        with open(filename + ".txt", 'w', encoding='UTF-8') as file_obj:
             for line in lines:
                 if line != '\n':
                     new_content = md.modify_text(line)
@@ -40,52 +42,48 @@ def SplitFiles():
         print(len(lines))
 
         # 读取文件
-        with open(filename + ".txt") as file_obj:
-                lines = file_obj.readlines()
+        with open(filename + ".txt", encoding='UTF-8') as file_obj:
+            lines = file_obj.readlines()
 
         # 一定要确认传入的锚点字符在文档里是唯一的，可以直接在文档里搜索来确认
-        spdots = [
-            'is known as a programming framework that lets developers build',
-            'we often forget how lucky we are to work in an industry where',
-            'Building an app is a journey of creating an initial skeleton of the app and',
-            'seeing it through to the end is not so easy',
-            'demoed by Ryan Dahl at JSConf in Berlin',
-            'different approaches to how they function under the hood',
-            'one of the first considerations is how the user will',
-            'They also focus on being accessible to the user without having',
-            'with using Microsoft Office will know how much functionality is available',
-            'The UI of an app is one of the most important things to get right',
-            'your computer and used to chat with friends and family',
-            'or configuring settings for how you use a particular app',
-            'Copying data from one source and using it in another is a function',
-            'shortcuts is invaluable for using apps in a fast and productive manner',
-            'open and running while focusing on one app at a time',
-            'trace that reports what error occurred and where it happened in the code',
-            'next step is to consider how you want to package it for running on your',
-            'and Electron space that simplify turning built Linux executables'
+        split_point_list = [
+            'wisdom about them can be better than individual wisdom',
+            'technological progress by establishing a backdrop of unknown',
+            'which I argue that in engineering, models are stacked many layers deep',
+            'ideas are more durable than the hardware itself',
+            'argue that the layers of paradigms for software are so deep',
+            'I argue that technology revolutions differ from scientific revolutions',
+            'which I examine the concept of information',
+            'I explain what software cannot do and show that the number',
+            'which I go beyond the countable world of computing and argue that',
+            'argue that determinism is a property of models not of the physical world',
+            'model of uncertainty about a system and not directly a model of that',
+            ' analyze what is holding back technology advancement',
+            'These are the main reasons I wrote this book'
         ]
 
         n1 = 101
         n2 = 1001
 
-        for chunk in chunks(lines, spdots):
+        for chunk in chunks(lines, split_point_list):
             if n1 < 1000:
-                with open(filename + "0" + str(n1-100) + ".md", 'w') as file_obj:
+                with open(filename + "0" + str(n1-100) + ".md", 'w', encoding='UTF-8') as file_obj:
                     for line in chunk:
                         if line != '\n':
                             file_obj.write(line + '\n')
                 n1 += 100
 
             else:
-                with open(filename + str(n2-100) + ".md", 'w') as file_obj:
+                with open(filename + str(n2-100) + ".md", 'w', encoding='UTF-8') as file_obj:
                     for line in chunk:
                         if line != '\n':
                             file_obj.write(line + '\n')
                     n2 += 100
 
+
 if __name__ == '__main__':
-    time1=time.time()
-    SplitFiles()
+    time1 = time.time()
+    split_files()
     time2 = time.time()
     print('OK!')
     print('Time Used: ' + str(time2 - time1) + 's')
